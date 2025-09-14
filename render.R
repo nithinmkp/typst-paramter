35c5287ec89f6942f43788540ff956c7c35962aa
Nithin M <65053643+nithinmkp@users.noreply.github.com>
2025-09-14 07:54:18 +0530

    full materials

--- render.R
+++ render.R
@@ -1,15 +1,64 @@
 library(quarto)
 library(dplyr)
 library(purrr)
+library(fs)
+library(glue)
+library(here)
+
+# penguins %>%
+# pull(species) %>%
+# unique() %>%
+# walk(
+# ~ quarto::quarto_render(
+# input = "penguins.qmd",
+# output_format = "typst",
+# output_file = paste0("penguins_", .x, ".pdf"),
+# execute_params = list(species = .x)
+#
+# )
+# )
+
+# Create output folder once
+dir_create("output")
 
 penguins %>%
     pull(species) %>%
     unique() %>%
-    map(
-        ~ quarto::quarto_render(
-            input = "penguins.qmd",
-            output_format = "typst",
-            output_file = paste0("penguins_", .x, ".pdf"),
-            execute_params = list(species = .x)
-        )
+    walk(
+        ~ {
+            outfile <- glue("penguins_{.x}.pdf")
+
+            quarto_render(
+                input = here("penguins.qmd"), # absolute project-root path
+                output_format = "pdf",
+                output_file = outfile,
+                execute_params = list(species = .x)
+            )
+
+            file_move(
+                path = here(outfile),
+                new_path = here("output", outfile)
+            )
+        }
+    )
+
+penguins %>%
+    pull(species) %>%
+    unique() %>%
+    walk(
+        ~ {
+            outfile <- glue("parent_penguins_{.x}.pdf")
+
+            quarto_render(
+                input = here("parent-penguins.qmd"), # absolute project-root path
+                output_format = "pdf",
+                output_file = outfile,
+                execute_params = list(species = .x)
+            )
+
+            file_move(
+                path = here(outfile),
+                new_path = here("output", outfile)
+            )
+        }
     )
--- typst-template.typ
+++ typst-template.typ
@@ -6,7 +6,7 @@
   author: "Author",
 
   // The book's author.
-  species: "default species",
+  species: "Adelie",
 
   // The paper size to use.
   paper: "a4",
@@ -14,49 +14,23 @@
   // The book's content.
   body,
 ) = {
-  // Define color schemes based on species
-  let colors = if species == "Adelie" {
-    (sidebar: rgb("#FF6B35"), header: rgb("#004E89"))
-  } else if species == "Chinstrap" {
-    (sidebar: rgb("#7209B7"), header: rgb("#F72585"))
-  } else if species == "Gentoo" {
-    (sidebar: rgb("#0D2C54"), header: rgb("#339989"))
-  } else {
-    // Default colors
-    (sidebar: rgb("#0D2C54"), header: rgb("#339989"))
-  }
+  
 
   // Set the document's metadata.
   set document(title: title, author: author)
 
   // Set the body font.
-  set text(font: "Source Sans Pro")
+  set text(font: "Cambria")
 
   // Configure the page properties.
   set page(
     paper: paper,
-    background: place(
-        top,
-        rect(
-            width: 2cm,
-            height: 100%,
-            fill: colors.sidebar
-        )
-    ),
+    
     margin: (bottom: 1cm, top: 1cm, left: 2.5cm, right: 2.5cm),
-  )
+    numbering: "1 of 1"
 
-  page(align(left + top)[
-    #h(-1cm)
-    #box(
-        fill: colors.header,
-        width: 0.7fr,
-        inset: 10pt,
-    )[
-        #text(size: 2.5em, fill: white)[*#title*]
-    ]
-    #v(2em, weak: true)
-    #text(size: 1.6em)[Species: ]
-    #body
-  ])
+  )
+  set par(justify: true)
+
+  body
 }
